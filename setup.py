fcec657bc04103d348e7ea11d2c96fd57911a1f6
JustADeer <shaquillesouzan3@gmail.com>
2026-07-13 07:14:18 +0700

    fix: handle new TOML and setup

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from skbuild import setup
+from setuptools import find_packages
 
 prefix = os.environ.get("CONDA_PREFIX") or getattr(sys, "prefix", None)
 cmake_args = []
@@ -8,6 +9,14 @@ if prefix:
     cmake_args.append(f"-DCMAKE_PREFIX_PATH={prefix}")
 
 setup(
-    packages=["osmordred"],
+    name="osmordred",
+    version="0.1.0",
+    description="osmordred-community",
+    packages=find_packages(),
+    python_requires=">=3.11",
+    install_requires=[
+        "numpy",
+        "rdkit",
+    ],
     cmake_args=cmake_args,
 )
